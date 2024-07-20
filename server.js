8126b701021d684d30fbd8583395a6cbf32d4910
Surbhi Singhal <surbhi98singhal.com>
2024-07-20 12:18:18 +0530

    second commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
+const mongoose = require('mongoose');
+const Product = require('./models/Product');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'uploads',
+    format: async (req, file) => 'png',
+    public_id: (req, file) => file.originalname,
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const username = process.env.MONGO_USERNAME;
+const password = encodeURIComponent(process.env.MONGO_PASSWORD);
+
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@cluster0.3j0ywmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+app.post('/api/products', upload.single('images'), async (req, res) => {
+
+  const itemData = req.body;
+  const item = new Product(itemData);
+  console.log("Data received");
+  try {
+    await item.save();
+    res.status(201).send(item);
+    console.log(item, "item");
+  } catch (e) {
+    res.status(400).send("Error saving data");
+    console.error(e);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
