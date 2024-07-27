e92eb861cc6ea49c566f67ecce19bf886693f5a9
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2024-07-28 01:32:03 +0530

    updated

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import { v2 as cloudinary } from 'cloudinary';
 import { connect } from 'mongoose';
 import Product from './models/Product.js';
 import 'dotenv/config';
+const PORT = 3000;
 
 const app = express();
 app.use(cors({
@@ -14,33 +12,6 @@ app.use(cors({
   headers: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'uploads',
-    format: async (req, file) => 'png',
-    public_id: (req, file) => file.originalname,
-  },
-});
-
-// const upload = multer({
-//   storage: storage,
-//   limits: { fileSize: 1024 * 1024 * 5 }, // 5MB
-//   fileFilter(req, file, cb) {
-//     if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
-//       cb(null, true);
-//     } else {
-//       cb(null, false);
-//     }
-//   }
-// });
 
 const username = process.env.MONGO_USERNAME;
 const password = encodeURIComponent(process.env.MONGO_PASSWORD);
@@ -55,65 +26,25 @@ connect(
 
 app.get('/prod', async (req, res) => {
   try {
-    const products = await Product.find().select(["-__v", "-_id"]);
-    console.log(products);
-    res.status(200).send(products);
-  } catch (e) {
-    console.error(e);
-    res.status(500).send("Error fetching products");
+    const users = await Product.find();
+    res.json(users);
+  } catch (error) {
+    console.error('Error', error);
+    res.status(500).json({ error: 'Failed' });
   }
 });
 
-
-// app.post('/products', upload.single('images'), async (req, res) => {
-//   try {
-//     const itemData = req.body;
-//     itemData.image = req.file.path; // add image path to itemData
-//     const item = new Product(itemData);
-//     console.log("Data received");
-//     await item.save();
-//     res.status(201).send(item);
-//     console.log(item, "item");
-//   } catch (e) {
-//     console.error(e);
-//     res.status(400).send("Error saving data");
-//   }
-// });
 app.post('/products', async (req, res) => {
   try {
-    const { name,description, price, image } = req.body;
-
-    if (!name || !price || !image || !description) {
-      return res.status(400).send('Missing required fields');
-    }
-    const item = new Product({ name, price, image, description });
-    await item.save();
-    res.status(201).send(item);
-  } catch (e) {
-    console.error(e);
-    res.status(400).send("Error saving data");
+    const { name, price, description, image } = req.body;
+    const user = new Product({ name, price, description, image });
+    await user.save();
+    res.json({ message: 'Products added successfully' });
+  } catch (error) {
+    console.error('Error', error);
+    res.status(500).json({ error: 'Failed' });
   }
 });
 
-app.put('/products/:id', async (req, res) => {
-  const { id } = req.params;
-  const updateData = req.body;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-  try {
-    const updateResult = await Product.updateOne({ _id: id }, updateData);
-    
-    if (updateResult.matchedCount === 0) {
-      return res.status(404).send('Product not found');
-    }
-    
-    const updatedProduct = await Product.findById(id);
-    res.status(200).send(updatedProduct);
-  } catch (e) {
-    console.error(e);
-    res.status(400).send("Error updating product");
-  }
-});
-
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
